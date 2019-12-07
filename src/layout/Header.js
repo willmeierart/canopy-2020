d8f991200f8a74888fb05447cda56e141d9f50bc
WILL MEIER <willmeierart@gmail.com>
2019-12-07 15:07:16 -0700

    Add routes, Header, graphql endpoint

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,3 +1,43 @@
-const Header = () => (<div />)
+import Link from 'next/link'
+import { routes } from 'server/routes'
+
+const Header = () => {
+	const { ABOUT, HOME } = routes
+
+	return (
+		<header>
+			<Link href={HOME.path}>
+				<img src="/static/images/logo.png" />
+			</Link>
+			<div>
+				{[/*PORTFOLIO,*/ ABOUT].map((route, i) => <Link key={`link-${i}`} href={route.path}><span>{route.title}</span></Link>)}
+			</div>
+			<style jsx>{`
+				header {
+					align-items: flex-end;
+					display: flex;
+					justify-content: space-between;
+					padding: 3rem 1rem 1rem 1rem
+				}
+				img {
+					cursor: pointer;
+					max-height: 2rem;
+				}
+				img:hover {
+					filter: brightness(400%);
+				}
+				span {
+					cursor: pointer;
+					margin-left: 1rem;
+				}
+				span:hover {
+					background-color: var(--color-background-hover);
+					color: var(--color-text-hover);
+					padding: 1px;
+				}
+			`}</style>
+		</header>
+	)
+}
 
 export default Header
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,1 +1,19 @@
-export default {}
+const routes = {
+	ABOUT: {
+		path: '/about',
+		page: 'about',
+		title: 'About'
+	},
+	HOME: {
+		path: '/',
+		page: 'index',
+		title: 'Home'
+	},
+	PORTFOLIO: {
+		path: '/portfolio',
+		page: 'portfolio',
+		title: 'Film & Animation'
+	},
+}
+
+exports.routes = routes
